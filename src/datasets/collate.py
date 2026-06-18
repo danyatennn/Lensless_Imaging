c81a1434db7272739b02dfcbaa353e202ceaf164
Daniil Ten <daten@edu.hse.ru>
2026-06-18 17:02:45 +0300

    implement DigiCam-Mirflickr dataset

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -2,24 +2,13 @@ import torch
 
 
 def collate_fn(dataset_items: list[dict]):
-    """
-    Collate and pad fields in the dataset items.
-    Converts individual items into a batch.
+    result_batch = {
+        "measurement": torch.stack([item["measurement"] for item in dataset_items]),
+        "psf": torch.stack([item["psf"] for item in dataset_items]),
+        "id": [item["id"] for item in dataset_items],
+    }
 
-    Args:
-        dataset_items (list[dict]): list of objects from
-            dataset.__getitem__.
-    Returns:
-        result_batch (dict[Tensor]): dict, containing batch-version
-            of the tensors.
-    """
-
-    result_batch = {}
-
-    # example of collate_fn
-    result_batch["data_object"] = torch.vstack(
-        [elem["data_object"] for elem in dataset_items]
-    )
-    result_batch["labels"] = torch.tensor([elem["labels"] for elem in dataset_items])
+    if "gt" in dataset_items[0]:
+        result_batch["gt"] = torch.stack([item["gt"] for item in dataset_items])
 
     return result_batch
